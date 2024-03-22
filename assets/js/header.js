024965583cf5d6f3b19184f99e2207868f6e6a0f
Sirinya <sry.ongkh@gmail.com>
2024-03-22 19:27:24 +0700

    gym-fitness-web-responsive

---
+++ assets/js/header.js
@@ -0,0 +1,2 @@
+import logo from "/";
+document.querySelector("#header").innerHTML = `<header class="">Test</header>`;
---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import "../css/style.css";
+import logo from "/logo.svg";
+
+document.querySelector("#main").innerHTML = `
+`;
